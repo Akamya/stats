23eca7bd548fc7a7479dcf999fe9146d3dbba83c
Akamya <elodie.langlet@gmail.com>
2024-09-23 14:21:18 +0200

    test2

--- tests/Unit/Functions/AverageTest.php
+++ tests/Unit/Functions/AverageTest.php
@@ -9,11 +9,5 @@ test("peux calculer la moyenne d'une liste", function () {
     expect(($result))->toBe(5.0);
 });
 
-test("N'accepte pas de lettre", function () {
-    $liste = [0,2,4,6,8,"a"];
-    $result = Average::calculate($liste);
-
-    expect(($result))->toBe(5.0);
-});
 
 ?>
